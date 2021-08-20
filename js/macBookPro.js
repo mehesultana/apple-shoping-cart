2c76fd59311ffb38b5ac580c022eb34f3d0c8341
mehesultana <sultanamehe01@gmail.com>
2021-08-21 03:10:40 +0600

    button add

--- js/macBookPro.js
+++ js/macBookPro.js
@@ -1,6 +1,13 @@
 //memory
-document.getElementById('unified-memory').addEventListener('click', function () {
+document.getElementById('unified-memory16').addEventListener('click', function () {
     const memoryCostText = document.getElementById('memory-cost');
     const memoryCost = memoryCostText.innerText;
     memoryCostText.innerText = parseFloat(memoryCost) + 180;
 });
+document.getElementById('unified-memory8').addEventListener('click', function () {
+    const memoryCostText = document.getElementById('memory-cost');
+    const memoryCost = memoryCostText.innerText;
+    memoryCostText.innerText = parseFloat(memoryCost) * 0;
+});
+
+//Storage
