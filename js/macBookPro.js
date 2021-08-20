570afc867e69513a2ad61da216519869325be337
mehesultana <sultanamehe01@gmail.com>
2021-08-21 03:44:09 +0600

    button add

--- js/macBookPro.js
+++ js/macBookPro.js
@@ -11,3 +11,36 @@ document.getElementById('unified-memory8').addEventListener('click', function ()
 });
 
 //Storage
+document.getElementById('storage-512gb').addEventListener('click', function () {
+    const storageCostText = document.getElementById('storage-cost');
+    const storageCost = storageCostText.innerText;
+    storageCostText.innerText = parseFloat(storageCost) + 100;
+});
+
+document.getElementById('storage-1tb').addEventListener('click', function () {
+    const storageCostText = document.getElementById('storage-cost');
+    const storageCost = storageCostText.innerText;
+    storageCostText.innerText = parseFloat(storageCost) + 180;
+});
+
+document.getElementById('storage-256gb').addEventListener('click', function () {
+    const storageCostText = document.getElementById('storage-cost');
+    const storageCost = storageCostText.innerText;
+    storageCostText.innerText = parseFloat(storageCost) * 0;
+});
+
+//delivery option
+
+document.getElementById('delivery-aug21').addEventListener('click', function () {
+    const deliveryChargeText = document.getElementById('delivery-charge');
+    const deliveryCharge = deliveryChargeText.innerText;
+    deliveryChargeText.innerText = parseFloat(deliveryCharge) + 20;
+});
+
+document.getElementById('delivery-aug25').addEventListener('click', function () {
+    const deliveryChargeText = document.getElementById('delivery-charge');
+    const deliveryCharge = deliveryChargeText.innerText;
+    deliveryChargeText.innerText = parseFloat(deliveryCharge) * 0;
+});
+
+//total
